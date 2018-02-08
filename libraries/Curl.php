67621b021424f3f70bb5dd16695435ffb8e74186
Vuong <vuongggggg@users.noreply.github.com>
2018-02-08 14:14:24 +0700

    Refactoring post request including multiple files.

--- libraries/Curl.php
+++ libraries/Curl.php
@@ -112,26 +112,25 @@ class Curl {
 	 * Use these methods to build up more complex queries
 	 * ================================================================================= */
 
-	public function post($params = array(), $options = array())
-	{
+	public function post($params = array(), $options = array(), $files = array())
+	{	
+		// Check fileinfo extension has been enabled when file upload already
+		if (!function_exists('finfo_open') && $files) {
+			log_message('error', 'You need enable fileinfo php extension before call post with file path param');
+			throw Exception('The fileinfo extension should be enabled');
+		}
+		
+		//Add files to request
+		foreach ($files as $key => &$value) {
+			if(file_exists($value)){
+				$finfo_open = finfo_open(FILEINFO_MIME_TYPE);
+				$file_mime = @finfo_file($finfo_open, $value);
+				$this->option(CURLOPT_POSTFIELDS, new CurlFile($value, $file_mime, end(explode('/', $value))));
+			}
+		}
+		
 		// If its an array (instead of a query string) then format it correctly
 		if (is_array($params)){
-			//Process file array if it exists
-			if(isset($params['file'])){
-				if(function_exists('finfo_open')){
-					foreach ($params['file'] as $key => &$value) {
-						if(file_exists($value)){
-							$finfo_open = finfo_open(FILEINFO_MIME_TYPE);
-							$file_mime = @finfo_file($finfo_open, $value);
-							$this->option(CURLOPT_POSTFIELDS, new CurlFile($value, $file_mime, end(explode('/', $value))));
-						}
-					}
-				} else{
-					log_message('error', 'You need enable fileinfo php extension before call post with file path param');
-				}
-				unset($params['file']);
-			}
-			
 			$params = http_build_query($params, NULL, '&');
 		}
 		// Add in the specific options provided
